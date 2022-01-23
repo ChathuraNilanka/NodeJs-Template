087d90f0f5a7d4b442234adde960dcadfe46e114
Chathura Nilanka <44176358+ChathuraNilanka@users.noreply.github.com>
2022-01-23 22:34:51 +0530

    Initial commit

---
+++ src/services/express.js
@@ -0,0 +1,39 @@
+/*
+ * Express Service
+ * v1.0.0
+ * Nova Solutions (Pvt) Ltd
+ *
+*/
+
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const path = require("path");
+
+const config = require('../config');
+const apiRouter = require('../routes/api');
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("combined"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, "../views")));
+
+app.use((req, res, next) => {
+    res.sendFile(path.resolve(__dirname, "../views/index.html"));
+});
+
+app.use("/api", apiRouter);
+
+exports.start = () => {
+    app.listen(config.port, config.host, (err) => {
+        if (err) {
+            console.log(`Error : ${err}`);
+            process.exit(1);
+        }
+        console.log(`${config.APP_NAME} Backend API - ${config.port}`);
+    });
+}
